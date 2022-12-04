9dc5518b10a9407017ec3596a8fadcfd97c187cc
Misan Etchie <msnetchie11@gmail.com>
2022-12-04 21:49:52 +0100

    Initial Commit

---
+++ Roadmap/RoadmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadmapApp.swift
+//  Roadmap
+//
+//  Created by Misan Etchie on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RoadmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
